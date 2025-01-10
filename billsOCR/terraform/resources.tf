a0b61708103cf24748c78133e0ba31ccfdbf5aa6
Hai Nguyen Xuan <nguyenxuanhai123456@gmail.com>
2025-01-10 22:44:32 +0700

    add database processes

--- billsOCR/terraform/resources.tf
+++ billsOCR/terraform/resources.tf
@@ -1,15 +1,93 @@
-resource "random_string" "bills_password" {
-    length = 10
-    upper = true
-    numeric = true
-    special = true
+#data "aws_availability_zones" "available" {}
+#resource "aws_vpc" "bills_vpc" {
+#    cidr_block = var.vpc_ip
+#    tags = {
+#        Name = "BillsOCR VPC"
+#    }
+#}
+#resource "aws_subnet" "public_subnets" {
+#    count = length(var.public_sub)
+#    vpc_id = "${aws_vpc.bills_vpc.id}"
+#    cidr_block = element(var.public_sub, count.index)
+#    availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+#}
+#resource "aws_db_subnet_group" "db_subnet_group" {
+#    name = "db_subnet_group"
+#    subnet_ids = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id]
+#}
+#resource "aws_subnet" "private_subnets" {
+#    count = length(var.private_sub)
+#    vpc_id = "${aws_vpc.bills_vpc.id}"
+#    cidr_block = element(var.private_sub, count.index)
+#}
+resource "random_password" "bills_password" {
+  length           = 16
+  special          = false
 }
+#resource "aws_security_group" "bills_security" {
+#  vpc_id      = "${aws_vpc.bills_vpc.id}"
+#  name        = "billsocr_security"
+#  description = "Allow all inbound for Postgres"
+#ingress {
+#    from_port   = 5432
+#    to_port     = 5432
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
 resource "aws_db_instance" "default" {
-    allocated_storage = 10
-    db_name = "billsOCR"
-    engine = "postgres"
-    instance_class = "db.t3.micro"
-    skip_final_snapshot = true
-    username = "bills"
-    password = "${random_string.bills_password.result}"
+  allocated_storage = 10
+  db_name = "billsOCR"
+  engine = "postgres"
+  instance_class = "db.t3.micro"
+  skip_final_snapshot = true
+  publicly_accessible = true
+  username = "bills"
+  password = random_password.bills_password.result
+#    vpc_security_group_ids = [aws_security_group.bills_security.id]
+#    db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
 }
+resource "aws_s3_bucket" "model_data"{
+  bucket = "billsocr-model-data"
+  force_destroy = true
+}
+#resource "mongodbatlas_project" "bills_project" {
+#  org_id = var.atlas_org_id
+#  name = "billsdb"
+#}
+#resource "mongodbatlas_database_user" "billsdb_user" {
+#  username = "bills"
+#  password = random_password.bills_password.result
+#  project_id = mongodbatlas_project.bills_project.id
+#  auth_database_name = "admin"
+#  roles {
+#    role_name     = "readWrite"
+#    database_name = "det_dataset"
+#  }
+#  roles {
+#    role_name     = "readWrite"
+#    database_name = "recog_dataset"
+#  }
+#}
+#resource "mongodbatlas_project_ip_access_list" "ip" {
+#  project_id = mongodbatlas_project.bills_project.id
+#  cidr_block = "0.0.0.0/0"
+#}
+#resource "mongodbatlas_cluster" "bills_cluster" {
+#  project_id   = mongodbatlas_project.bills_project.id
+#  name         = "bills-cluster"
+#  cluster_type = "REPLICASET"
+#  replication_specs {
+#    num_shards = 1
+#    regions_config {
+#      region_name     = upper(var.region)
+#      electable_nodes = 3
+#      priority        = 7
+#      read_only_nodes = 0
+#    }
+#  }
+#  # Provider Settings "block"
+#  provider_instance_size_name = "M0"
+#  provider_name               = "TENANT"
+#  backing_provider_name       = "AWS"
+#}
